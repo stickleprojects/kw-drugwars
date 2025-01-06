2b54c96c8dca9e93564d65e93b6dd9df455d09b3
Kieron Wray <kieron.wray@gmail.com>
2025-01-06 21:51:44 +0000

    initial commnit and working on updating qty and amount in wallet

---
+++ kw-drugwars/src/components/City.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="citydiv">
+        <h1 class="cityname">{{city.name}}</h1>
+        <h2 class="productlistlabel">Products & Prices</h2>
+        <b-table class="productlist" striped hover :items="city.products" :fields="fields" primary-key="id">
+            <template v-slot:cell(buy)="{ item }">
+                <span><b-btn @click="buyItem(item)">buy</b-btn></span>
+                
+            </template>
+           
+        </b-table>
+</div>
+</template>
+
+<style>
+.productlistlabel {
+    text-align: left;
+    font-size: 1em;
+}
+.productlist {
+    width: 75%;
+}
+.citydiv {
+    border: 1px solid black;
+    box-shadow: 10px 10px;
+    margin: 40px;
+    padding: 10px;
+    width: 50%;
+    font-family: 'Courier New', Courier, monospace;
+}
+
+.cityname {
+    text-align: left;
+    font-size: 2em;
+}
+</style>
+
+<script>
+
+export default {
+    name:"city-info",
+    props: {
+        'buyitem': 
+        'user': {
+            name: String,
+            balance: Number
+        },
+        'city': {
+            name: String,
+            products:[
+                {id: String, name: String, price: Number, quantity:Number}
+            ]
+        }
+    },
+    methods: {
+        buyItem(item) {
+            let x=prompt("how many");
+            var qty = parseInt(x);
+            if(isNaN(qty)) {
+                return;
+            }
+            console.log("buying %d items", qty);
+
+            if(qty > item.quantity) {
+                alert("thats too many, we only have " + item.quantity +" available!");
+                return;
+            }
+
+            const total =qty * item.price;
+            if(total > this.user.balance) {
+                alert("Sorry " + this.user.name +", but you cant afford that, its " + total +" and you only have " + this.user.balance);
+                return;
+            }
+            console.log(item.id);
+        }
+    },
+    data() {
+        return {
+        
+        fields:[
+        
+            {key:"name", sortable: true, tdClass:"text"},
+            {key:"quantity", sortable: true, tdClass:"price", label:"Quantity you can buy"},
+            {key:"price", sortable: true, tdClass:"price"},
+            {key:"buy", sortable: true, tdClass:"price"},
+            
+        ],
+        }
+    }
+}
+</script>
---
+++ kw-drugwars/src/components/HelloWorld.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    
+    <div>
+      <div v-for="city in cities" v-bind:key="city.name">
+        <City :city="city" :user="user" :buyitem=""/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import City from './City.vue';
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components: {
+    City
+  },
+  methods: {
+    buyItem(id, qty, price) {
+
+    }
+  }
+  data() {
+    return {
+      user: {
+        name:"kieron",
+        balance:214.34
+      },
+      cities:[
+      { name:"london", products:[
+        { id: 1, name: "heroin", price: 23.23, quantity: 23}
+      ]},
+      { name:"new york", products:[
+        { id: 1, name: "heroin", price: 93.23, quantity: 33},
+        { id: 2, name: "coke", price: 113.23, quantity: 100},
+        { id: 3, name: "peanuts", price: 116.23, quantity: 100}
+      ]},
+      
+    ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+.price {
+  text-align: right;
+  
+}
+.text {
+  text-align: left;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
