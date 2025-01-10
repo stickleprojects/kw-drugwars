f843d4d9f2ce4a9d93cd533c50f3dee6fc329e01
kieron wray <kieron.wray@gmail.com>
2025-01-10 18:03:56 +0000

    wip : added linechart

--- src/components/ProfitChart.vue
+++ src/components/ProfitChart.vue
@@ -1,14 +1,26 @@
 <template>
-    <b-card title="Balance over time" class="shadow p-3 mb-5 bg-white rounded">
+<div>
+<h1>line chart here</h1>
+<vline :data="chartdata"/>
 
+<h1> line chart above here</h1>
+</div>
 
-    </b-card>
 </template>
 
 
 <script setup>
 import { drugDataStore } from '@/datastore';
+import { Line  as vline } from 'vue-chartjs';
+ import { CategoryScale, Chart as ChartJS,LinearScale, PointElement, LineElement, Tooltip  } from 'chart.js';
 
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Tooltip
+)
 </script>
 
 <script>
@@ -23,7 +35,19 @@ export default {
         return {
             drugStore: drugDataStore(),
 
+            chartdata:{
+            labels:['jan','feb','mar'],
+            datasets:[
+                {
+                    label:'data one',
+                    data:[40,30,100]
+                }
+            ]
+            }
         }
+    },
+    components: {
+        vline
     }
 }
 </script>
