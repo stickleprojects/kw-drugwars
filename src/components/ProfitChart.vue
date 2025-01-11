5e3f3a789150e11192dee54abdbe99bb40cf2ac0
kieron wray <kieron.wray@gmail.com>
2025-01-11 17:48:18 +0000

    fixed chart size

--- src/components/ProfitChart.vue
+++ src/components/ProfitChart.vue
@@ -1,55 +1,103 @@
 <template>
-<div>
-<h1>line chart here</h1>
-<vline :data="chartdata"/>
+  <div>
+    <b-card title="profits">
+      <v-container class="profitChart">
+        <vline :data="chartdata" :options="chartOptions" />
+      </v-container>
 
-<h1> line chart above here</h1>
-</div>
+    </b-card>
+
+  </div>
 
 </template>
 
 
 <script setup>
 import { drugDataStore } from '@/datastore';
-import { Line  as vline } from 'vue-chartjs';
- import { CategoryScale, Chart as ChartJS,LinearScale, PointElement, LineElement, Tooltip  } from 'chart.js';
+import { Line as vline } from 'vue-chartjs';
+import { CategoryScale, Chart as ChartJS, LinearScale, PointElement, LineElement, Tooltip } from 'chart.js';
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Tooltip
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip
 )
 </script>
 
 <script>
 
 export default {
-    name: "city-info",
-    props: {
-        'user': Object,
+  name: "city-info",
+  props: {
+    'user': Object,
 
-    },
-    data() {
-        return {
-            drugStore: drugDataStore(),
+  },
+  data() {
+    return {
+      drugStore: drugDataStore(),
 
-            chartdata:{
-            labels:['jan','feb','mar'],
-            datasets:[
-                {
-                    label:'data one',
-                    data:[40,30,100]
-                }
-            ]
-            }
-        }
-    },
-    components: {
-        vline
+      chartdata: {
+        labels: ['jan', 'feb', 'mar'],
+        datasets: [
+          {
+            label: 'data one',
+            data: [40, 30, 100]
+          }
+        ]
+      },
+      chartOptions: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+            },
+            gridLines: {
+              display: true,
+            },
+          }],
+          xAxes: [{
+            ticks: {
+              beginAtZero: true,
+            },
+            gridLines: {
+              display: false,
+            },
+          }],
+        },
+        legend: {
+          display: true,
+        },
+        tooltips: {
+          enabled: true,
+          mode: 'single',
+          callbacks: {
+            label(tooltipItems, data) {
+              const { datasetIndex, index } = tooltipItems;
+              const value = data.datasets[datasetIndex].data[index];
+              if (parseInt(value, 10) > 999) {
+                return ` ${value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
+              }
+              return ` ${value}`;
+            },
+          },
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        height: 300,
+      },
     }
+  },
+  components: {
+    vline
+  }
 }
 </script>
 
-<style></style>
+<style>
+.profitChart {
+  height: 150px;
+  display: block;
+}
+</style>
